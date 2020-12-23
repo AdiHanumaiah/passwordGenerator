37858c18644fae55fef8b24be780ce832610132f
AdiHanumaiah <76495969+AdiHanumaiah@users.noreply.github.com>
2020-12-22 21:14:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+//characters that can be used in the password
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
 
+//gets the characters 
+function getLowercase() {
+	return lowerLetters[Math.floor(Math.random() * lowerletters.length)];
+}
+
+function getUppercase() {
+	return upperLetters[Math.floor(Math.random() * upperletters.length)];
+}
+
+function getNumbers() {
+	return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbols() {
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+//generate password
